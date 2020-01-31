11e87ed6b8c3a51fd45170d04bec09e03c3eba52
Dev Bahl <devbahl12@gmail.com>
2020-01-31 22:35:55 +0530

    icon

--- TimerApp/ContentView.swift
+++ TimerApp/ContentView.swift
@@ -16,50 +16,57 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView{
-            VStack{
-                
-        Text(secondsToMinutesAndSeconds (seconds: timeManager.secondsLeft))
-            .font(.system(size: 80))
-            .padding(.top, 80)
-                
-        Image(systemName: timeManager.timerMode == .running ?
-            "pause.circle.fill":"play.circle.fill")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 180, height: 180)
-            .foregroundColor(.orange)
-            .onTapGesture(perform: {
-                if self.timeManager.timerMode == .initial{
-                    self.timeManager.setTimerLength(minutes:
-                        self.availableminutes[self.selectedPickerIndex]*60)
-                }
-                self.timeManager.timerMode == .running ?
-                self.timeManager.pause() :
-                self.timeManager.start()
-            })
-                
-                if timeManager.timerMode == .paused{
-            Image(systemName: "gobackward")
+                VStack{
+                    
+            Text(secondsToMinutesAndSeconds (seconds: timeManager.secondsLeft))
+                .font(.system(size: 80))
+                .padding(.top, 80)
+                    
+            Image(systemName: timeManager.timerMode == .running ?
+                "pause.circle.fill":"play.circle.fill")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: 50, height: 50)
+                .frame(width: 180, height: 180)
                 .foregroundColor(.orange)
-                .padding(.top, 40)
                 .onTapGesture(perform: {
-                    self.timeManager.reset()
+                    if self.timeManager.timerMode == .initial{
+                        self.timeManager.setTimerLength(minutes:
+                            self.availableminutes[self.selectedPickerIndex]*60)
+                    }
+                    self.timeManager.timerMode == .running ?
+                    self.timeManager.pause() :
+                    self.timeManager.start()
                 })
+                    
+                    if timeManager.timerMode == .paused{
+                Image(systemName: "gobackward")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 50, height: 50)
+                    .foregroundColor(.orange)
+                    .padding(.top, 40)
+                    .onTapGesture(perform: {
+                        self.timeManager.reset()
+                    })
+                    }
+                    if timeManager.timerMode == .initial{
+                 Picker(selection: $selectedPickerIndex, label: Text("")){
+                    ForEach(0..<availableminutes.count){
+                        Text("\(self.availableminutes[$0]) min")
+                    }
+                    }
+                    .labelsHidden()
+                    }
+                    Spacer()
                 }
-                if timeManager.timerMode == .initial{
-             Picker(selection: $selectedPickerIndex, label: Text("")){
-                ForEach(0..<availableminutes.count){
-                    Text("\(self.availableminutes[$0]) min")
-                }
-                }
-                .labelsHidden()
-                }
-                Spacer()
-            }
-        .navigationBarTitle("My Timer")
+                .navigationBarTitle("My Timer")
+                .navigationBarItems(trailing:
+                    Image(systemName: "timer")
+                    .resizable()
+                    .frame(width: 40,height: 40)
+                        .padding(.horizontal, 170)
+                        .padding(.top, 95)
+            )
         }
     }
 }
